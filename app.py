887ef1e82c243b1c8469af47c5de7888c1b5dacb
chetanshavanti <chetanshavanti27@gmail.com>
2025-12-21 18:38:55 +0530

    Git ignore added 3

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
+
 # Don't have a css file, so I will inject it directly
 st.markdown("""
 <style>
